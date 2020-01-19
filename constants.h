6372a8aa5f79e74fdc338f757b0cac1b47ec6f81
Umbranoctuna <60070238+Umbranoctuna@users.noreply.github.com>
2020-01-19 17:23:41 +0100

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,23 @@
+#ifndef constants_h
+#define constants_h
+
+#define FALSE 0
+#define TRUE 1
+
+//FPS
+#define FPS 60
+#define FRAME_TARGET_TIME (1000 / FPS)
+
+//Dimensions of the Window
+int WIDTH = 1280;
+int HEIGHT = 720;
+
+//Size of a Square
+#define SQUARE_SIZE 40
+
+//Size in 40x40 Pixels
+#define SQUARE_HEIGHT (HEIGHT / SQUARE_SIZE)
+#define SQUARE_WIDTH 32
+
+
+#endif /* constants_h */
